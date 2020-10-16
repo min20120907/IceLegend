04ff664908be0faa1e87bb9db112c1899536113f
BuildTools <unconfigured@null.spigotmc.org>
2020-10-17 01:32:47 +0800

    remove some stuffs and free dirt

--- src/com/icelegend/CommandIceLegend.java
+++ src/com/icelegend/CommandIceLegend.java
@@ -12,54 +12,48 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import de.themoep.inventorygui.DynamicGuiElement;
-import de.themoep.inventorygui.GuiElement;
-import de.themoep.inventorygui.GuiPageElement;
-import de.themoep.inventorygui.GuiPageElement.PageAction;
-import de.themoep.inventorygui.GuiStateElement;
-import de.themoep.inventorygui.GuiStorageElement;
-import de.themoep.inventorygui.InventoryGui;
-import de.themoep.inventorygui.StaticGuiElement;
+
 
 @SuppressWarnings("unused")
 public class CommandIceLegend implements CommandExecutor {
 
-
+	
 
 	@Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (sender instanceof Player) {
-            String[] guiSetup = {
-                    "  s i z  ",
-                    "  ggggg  ",
-                    "  fpdnl  "
-                };
-
 			Player player = (Player) sender;
-            // Here we need to give items to our player
-        	InventoryGui gui = new InventoryGui(null, ((Player) sender).getInventory().getHolder(), "Test UI", guiSetup );
-        	gui.addElement(new StaticGuiElement('s',
-        	        new ItemStack(Material.REDSTONE),
-        	        42, // Display a number as the item count
-        	        click -> {
-        	            if (click.getEvent().getWhoClicked().getName().equals("Redstone")) {
-        	                click.getEvent().getWhoClicked().sendMessage(ChatColor.RED + "I am Redstone!");
-        	                return true; // returning true will cancel the click event and stop taking the item
-        	            }
-        	            return false; // returning false will not cancel the initial click event to the gui
-        	        },
-        	        "You can add lines describing this element here!",
-        	        "The first line is displayed as the displayname,",
-        	        "any additional ones as the lore!",
-        	        "Any text the ItemStack had will be overwritten."
-        	));
+			Inventory gui = Bukkit.createInventory(null, 9,ChatColor.AQUA + "冰晶系統 "+ ChatColor.DARK_GRAY + ">> "+ "FREE DIRT" );
+			   
 
-
+	        //This is where you create the item
+	        ItemStack survival = new ItemStack (Material.DIRT);
+	        ItemMeta survivalMeta = survival.getItemMeta();
+	       
+	       
+	        //This is where you set the display name of the item
+	        survival.setItemMeta(survivalMeta);
+	       
+	        //This is where you decide what slot the item goes into
+	        gui.setItem(0, survival);
+	        gui.setItem(1, survival);
+	        gui.setItem(2, survival);
+	        gui.setItem(3, survival);
+	        gui.setItem(4, survival);
+	        gui.setItem(5, survival);
+	        gui.setItem(6, survival);
+	        gui.setItem(6, survival);
+	        gui.setItem(7, survival);
+	        gui.setItem(8, survival);
+	       
+	        //This opens the inventory
+	        player.openInventory(gui);
         	sender.sendMessage("[冰晶傳說] 打開界面中 by "+player.getDisplayName());
 
-        	gui.show(player);
+ 
             
         }else {
         	sender.sendMessage("[冰晶傳說] 請使用玩家身份使用此指令");
