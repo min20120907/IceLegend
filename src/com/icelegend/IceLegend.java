2af1b00008bee7d42fa0d8f36c7862df85044d1b
CangyuLin <jefflin.je598@gmail.ocm>
2020-10-21 06:23:30 +0800

    Add

--- src/com/icelegend/IceLegend.java
+++ src/com/icelegend/IceLegend.java
@@ -6,13 +6,14 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class IceLegend extends JavaPlugin{
+	private File msg = new File(this.getDataFolder(), "Message.yml");
+	FileConfiguration msg_config = YamlConfiguration.loadConfiguration(msg);
     @Override
     public void onEnable() {
         // Register our command "kit" (set an instance of your command class as executor)
-    	private File default_ = new File(this.getDataFolder(), "UIList.yml");
-    	FileConfiguration config = YamlConfiguration.loadConfiguration(default_);
+    	
         this.getCommand("ice").setExecutor(new CommandIceLegend());
-        getLogger().info("[冰晶傳說] 插件載入中...");
+        getLogger().info(msg_config.getString("load_plugin"));
     }
     // Fired when plugin is disabled
     @Override
