07fb8634c4b7855e7b6f87465897e58d59e48fd1
Cangyu Lin <jefflin.je598@gmail.com>
2020-12-08 09:28:46 +0800

    Add Entity classifiers

--- src/com/icelegend/IceLegend.java
+++ src/com/icelegend/IceLegend.java
@@ -2,8 +2,10 @@ package com.icelegend;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
@@ -15,18 +17,24 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityCategory;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Mob;
+import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerItemHeldEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.*;
 
 public class IceLegend extends JavaPlugin implements Listener {
 
-	
 	// initialize the yaml files
 	public File msg = new File(this.getDataFolder(), "Messages.yml");
 	public File attr_gui = new File(this.getDataFolder(), "AttributeGUI.yml");
@@ -61,10 +69,11 @@ public class IceLegend extends JavaPlugin implements Listener {
 	public FileConfiguration placeholder_config = YamlConfiguration.loadConfiguration(placeholder);
 	public FileConfiguration series_lore_config = YamlConfiguration.loadConfiguration(series_lore);
 	public FileConfiguration help_config = YamlConfiguration.loadConfiguration(help);
-	//components elements
+	// components elements
 	public ArrayList<ItemStack> components = new ArrayList<ItemStack>();
 	public ArrayList<ItemMeta> components_meta = new ArrayList<ItemMeta>();
-	public String[] ComponentType = {"Melee", "Magic", "Shoot", "Defence"};
+	public String[] ComponentType = { "Melee", "Magic", "Shoot", "Defence" };
+
 	@Override
 	public void onEnable() {
 		// if the YAML disappear, generate one.
@@ -122,7 +131,7 @@ public class IceLegend extends JavaPlugin implements Listener {
 		this.getCommand("itemskincombinegui").setExecutor(new CommandItemSkinCombineGUI(this));
 		this.getCommand("gemcombinegui").setExecutor(new CommandGemCombineGUI(this));
 		this.getCommand("skillbar").setExecutor(new CommandSkillBar(this));
-		//new commands
+		// new commands
 		this.getCommand("getitem").setExecutor(new CommandGetItem(this));
 		this.getCommand("getgem").setExecutor(new CommandGetGem(this));
 		this.getCommand("getskin").setExecutor(new CommandGetSkin(this));
@@ -136,31 +145,71 @@ public class IceLegend extends JavaPlugin implements Listener {
 		Bukkit.getPluginManager().registerEvents(new ClickerEvent(this), this);
 
 		// Register the items...
-		
-		
+
 		getLogger().info(format(msg_config.getString("Messages.loadplugin")));
 	}
+
 	// The event listener that will be triggered, when the player switch its item.
-    public void onItemSwitch(PlayerItemHeldEvent event) {
-    	Player p = event.getPlayer();
-    	ItemStack item = p.getInventory().getItem(event.getNewSlot());
-    	ItemMeta im  = item.getItemMeta();
-    	List<String> attr_list = (List<String>) attr_name_config.getList("attribute");
-    	AttributeModifier mod = new AttributeModifier(UUID.randomUUID(), attr_list.get(0),0.0, Operation.ADD_NUMBER, EquipmentSlot.HAND);
-    	for (String s: attr_list) {
-    		switch(s) {
-    		case "attack":
-    			s = "generic.attack_damage";
-    			break;
-    		case "attack_speed":
-    			s = "generic.attack_speed";
-    			break;
-    		
-    		}
-    	}
-    	
-    	im.addAttributeModifier(Attribute.GENERIC_FLYING_SPEED, mod);
-    }
+	public void onItemSwitch(PlayerItemHeldEvent event) {
+		Player p = event.getPlayer();
+		ItemStack item = p.getInventory().getItem(event.getNewSlot());
+		ItemMeta im = item.getItemMeta();
+		List<String> attr_list = (List<String>) attr_name_config.getList("attribute");
+
+		for (String s : attr_list) {
+			switch (s) {
+			case "attack":
+				s = "generic.attack_damage";
+				break;
+			case "attack_speed":
+				s = "generic.attack_speed";
+				break;
+			case "piercing":
+				break;
+			case "magic":
+				break;
+			case "shoot":
+				break;
+			case "range":
+				break;
+			case "mana_cost":
+				break;
+			case "":
+			}
+		}
+		AttributeModifier mod = new AttributeModifier(UUID.randomUUID(), attr_list.get(0),
+				components_yml_config.getDouble("" + attr_list.get(0)), Operation.ADD_NUMBER, EquipmentSlot.HAND);
+		im.addAttributeModifier(Attribute.GENERIC_FLYING_SPEED, mod);
+	}
+
+	public void onEntityDamage(EntityDamageByEntityEvent event) {
+		Entity victim = event.getEntity();
+		Entity damager = event.getDamager();
+		if (damager instanceof Player) {
+			Player p = (Player) damager;
+			ItemStack item = p.getInventory().getItemInMainHand();
+			Map<Attribute, Collection<AttributeModifier>> attr = item.getItemMeta().getAttributeModifiers().asMap();
+
+			if (victim instanceof Piglin || victim instanceof MagmaCube || victim instanceof WitherSkeleton
+					|| victim instanceof Strider || victim instanceof Blaze || victim instanceof PiglinBrute
+					|| victim instanceof Zoglin || victim instanceof Hoglin) // Nether
+			{
+
+			} else if (victim instanceof Mob) { // Mob
+
+			} else if (victim instanceof Player) { // Player
+
+			} else if (victim instanceof LivingEntity) {
+				LivingEntity v = (LivingEntity) victim;
+				if (v.getCategory().equals(EntityCategory.ARTHROPOD)) { // Arthropod
+
+				} else if (v.getCategory().equals(EntityCategory.UNDEAD)) { // Undead
+
+				}
+			}
+		}
+	}
+
 	// Fired when plugin is disabled
 	@Override
 	public void onDisable() {
@@ -171,53 +220,57 @@ public class IceLegend extends JavaPlugin implements Listener {
 	public String format(String str) {
 		return ChatColor.translateAlternateColorCodes('&', str.replace("%prefix%", msg_config.getString("prefix")));
 	}
+
 	@Override
-    public void reloadConfig() {
-        //config is a FileConfiguration object
-       //cfile is the File object
-    	// initialize the yaml files
-    	 msg = new File(this.getDataFolder(), "Messages.yml");
-    	 attr_gui = new File(this.getDataFolder(), "AttributeGUI.yml");
-    	 attr_name = new File(this.getDataFolder(), "AttributeName.yml");
-    	 class_yml = new File(this.getDataFolder(), "Class.yml");
-    	 class_gui = new File(this.getDataFolder(), "ClassGUI.yml");
-    	 components_yml = new File(this.getDataFolder(), "Components.yml");
-    	 gem = new File(this.getDataFolder(), "Gem.yml");
-    	 gem_com = new File(this.getDataFolder(), "GemCombineGUI.yml");
-    	 item_com = new File(this.getDataFolder(), "ItemCombineGUI.yml");
-    	 item_mat = new File(this.getDataFolder(), "ItemMaterial.yml");
-    	 item_skin = new File(this.getDataFolder(), "ItemSkin.yml");
-    	 item_skin_com = new File(this.getDataFolder(), "ItemSkinCombineGUI.yml");
-    	 item_tem = new File(this.getDataFolder(), "ItemTemplate.yml");
-    	 placeholder = new File(this.getDataFolder(), "Placeholder_List.yml");
-    	 series_lore = new File(this.getDataFolder(), "SeriesLore.yml");
-    	 help = new File(this.getDataFolder(), "Commandpage.yml");
-    	// initialize the yaml configurations
-    	msg_config = YamlConfiguration.loadConfiguration(msg);
-    	 attr_gui_config = YamlConfiguration.loadConfiguration(attr_gui);
-    	 attr_name_config = YamlConfiguration.loadConfiguration(attr_name);
-    	 class_yml_config = YamlConfiguration.loadConfiguration(class_yml);
-    	 class_gui_config = YamlConfiguration.loadConfiguration(class_gui);
-    	 components_yml_config = YamlConfiguration.loadConfiguration(components_yml);
-    	 gem_config = YamlConfiguration.loadConfiguration(gem);
-    	 gem_com_config = YamlConfiguration.loadConfiguration(gem_com);
-    	 item_com_config = YamlConfiguration.loadConfiguration(item_com);
-    	 item_mat_config = YamlConfiguration.loadConfiguration(item_mat);
-    	 item_skin_config = YamlConfiguration.loadConfiguration(item_skin);
-    	 item_skin_com_config = YamlConfiguration.loadConfiguration(item_skin_com);
-    	 item_tem_config = YamlConfiguration.loadConfiguration(item_tem);
-    	 placeholder_config = YamlConfiguration.loadConfiguration(placeholder);
-    	 series_lore_config = YamlConfiguration.loadConfiguration(series_lore);
-    	 help_config = YamlConfiguration.loadConfiguration(help);
-   }
+	public void reloadConfig() {
+		// config is a FileConfiguration object
+		// cfile is the File object
+		// initialize the yaml files
+		msg = new File(this.getDataFolder(), "Messages.yml");
+		attr_gui = new File(this.getDataFolder(), "AttributeGUI.yml");
+		attr_name = new File(this.getDataFolder(), "AttributeName.yml");
+		class_yml = new File(this.getDataFolder(), "Class.yml");
+		class_gui = new File(this.getDataFolder(), "ClassGUI.yml");
+		components_yml = new File(this.getDataFolder(), "Components.yml");
+		gem = new File(this.getDataFolder(), "Gem.yml");
+		gem_com = new File(this.getDataFolder(), "GemCombineGUI.yml");
+		item_com = new File(this.getDataFolder(), "ItemCombineGUI.yml");
+		item_mat = new File(this.getDataFolder(), "ItemMaterial.yml");
+		item_skin = new File(this.getDataFolder(), "ItemSkin.yml");
+		item_skin_com = new File(this.getDataFolder(), "ItemSkinCombineGUI.yml");
+		item_tem = new File(this.getDataFolder(), "ItemTemplate.yml");
+		placeholder = new File(this.getDataFolder(), "Placeholder_List.yml");
+		series_lore = new File(this.getDataFolder(), "SeriesLore.yml");
+		help = new File(this.getDataFolder(), "Commandpage.yml");
+		// initialize the yaml configurations
+		msg_config = YamlConfiguration.loadConfiguration(msg);
+		attr_gui_config = YamlConfiguration.loadConfiguration(attr_gui);
+		attr_name_config = YamlConfiguration.loadConfiguration(attr_name);
+		class_yml_config = YamlConfiguration.loadConfiguration(class_yml);
+		class_gui_config = YamlConfiguration.loadConfiguration(class_gui);
+		components_yml_config = YamlConfiguration.loadConfiguration(components_yml);
+		gem_config = YamlConfiguration.loadConfiguration(gem);
+		gem_com_config = YamlConfiguration.loadConfiguration(gem_com);
+		item_com_config = YamlConfiguration.loadConfiguration(item_com);
+		item_mat_config = YamlConfiguration.loadConfiguration(item_mat);
+		item_skin_config = YamlConfiguration.loadConfiguration(item_skin);
+		item_skin_com_config = YamlConfiguration.loadConfiguration(item_skin_com);
+		item_tem_config = YamlConfiguration.loadConfiguration(item_tem);
+		placeholder_config = YamlConfiguration.loadConfiguration(placeholder);
+		series_lore_config = YamlConfiguration.loadConfiguration(series_lore);
+		help_config = YamlConfiguration.loadConfiguration(help);
+	}
+
 	// Attributes String proclaiming
 	public String getAttrDesc(FileConfiguration c, String str) {
-		return attr_name_config.getString("Attribute."+c.getString(str));
+		return attr_name_config.getString("Attribute." + c.getString(str));
 	}
+
 	List<String> formatList(List<String> a) {
 		List<String> tmp = new ArrayList<String>();
 		for (int i = 0; i < a.size(); i++)
 			tmp.add(format(a.get(i)));
 		return tmp;
 	}
+
 }
